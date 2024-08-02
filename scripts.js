9ff3c320dbe22e89836b968ce5f305d880a5c4e7
Fureko <109041500+Fureko@users.noreply.github.com>
2024-08-02 12:54:38 +0200

    Chap 1 à 20

--- scripts.js
+++ scripts.js
@@ -12,6 +12,11 @@ document.addEventListener("DOMContentLoaded", () => {
         },
         "Roses and Champagne": {
             chapters: Array.from({ length: 52 }, (_, i) => i + 1)  // Chapitres numérotés de 1 à 52
+        },
+        "Oshi No Ko": {
+            chapters: [
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 20.5, 21, 22, 23, 24, 25, 25.5, 25.6, 25.7, 25.8, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 90.1, 90.2, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 111.1, 112, 113, 114, 115, 115.5, 116, 118, 119, 120, 121, 122, 123, 124, 125, 125.5, 125.6, 125.7, 125.8, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156
+            ]   
         }
     };
 
@@ -43,19 +48,22 @@ document.addEventListener("DOMContentLoaded", () => {
         let hasImages = false;
 
         while (true) {
-            const imgSrc = `Webtoon/${webtoonName}/${chapterName}/${String(index).padStart(2, '0')}.jpg`;
-            const imgSrcFallback = `Webtoon/${webtoonName}/${chapterName}/${String(index).padStart(2, '0')}.jpeg`;
+            // Vérifiez les fichiers PNG et JPG
+            const imgSrcPng = `Webtoon/${webtoonName}/${chapterName}/${String(index).padStart(2, '0')}.png`;
+            const imgSrcJpg = `Webtoon/${webtoonName}/${chapterName}/${String(index).padStart(2, '0')}.jpg`;
+            const imgSrcFallbackPng = `Webtoon/${webtoonName}/${chapterName}/${String(index).padStart(2, '0')}.jpeg`;
 
-            const imgExists = await imageExists(imgSrc);
-            const fallbackExists = !imgExists && await imageExists(imgSrcFallback);
+            const imgExistsPng = await imageExists(imgSrcPng);
+            const imgExistsJpg = await imageExists(imgSrcJpg);
+            const fallbackExists = !imgExistsJpg && await imageExists(imgSrcFallbackPng);
 
-            if (!imgExists && !fallbackExists) {
+            if (!imgExistsPng && !imgExistsJpg && !fallbackExists) {
                 break;
             }
 
             hasImages = true;
             const img = document.createElement('img');
-            img.src = imgExists ? imgSrc : imgSrcFallback;
+            img.src = imgExistsPng ? imgSrcPng : imgExistsJpg ? imgSrcJpg : imgSrcFallbackPng;
             img.alt = `Page ${index}`;
             imagesDiv.appendChild(img);
 
