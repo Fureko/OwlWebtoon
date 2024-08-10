1ca8b7c5deb0aff2ced338d5825a32097e025bcd
Fureko <furekoereki@gmail.com>
2024-08-10 19:52:05 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -153,5 +153,18 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 
+    // Ajout du bouton "Remonter en haut de la page"
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 300) {
+            scrollToTopBtn.style.display = "block";
+        } else {
+            scrollToTopBtn.style.display = "none";
+        }
+    });
+
+    scrollToTopBtn.addEventListener("click", () => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+
     initializeWebtoonButtons();
 });
